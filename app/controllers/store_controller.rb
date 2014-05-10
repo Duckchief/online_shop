9c4ee6698c785f644f1ba238c7e5f21d616a8f41
David Ng <david@immanuelmedia.com>
2014-05-10 15:00:40 +0800

    included created column

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,4 +2,8 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
   end
+
+  def recent
+    @products = Product.order(created_at: :desc).limit(10)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ OnlineShop::Application.routes.draw do
   root 'store#index'
 
   resources :products
+  get '/recent' => 'store#recent'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
